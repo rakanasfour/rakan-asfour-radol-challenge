c96e5b7e2750bb477153d53e6d23de1a09852c0c
Rakan Asfour <102238945+rakanasfour@users.noreply.github.com>
2024-07-17 04:13:12 -0700

    Update variables.tf

--- terraform/modules/rds/variables.tf
+++ terraform/modules/rds/variables.tf
@@ -1,24 +1,82 @@
-variable "db_name" {
-  description = "Database name"
+variable "allocated_storage" {
+  description = "The allocated storage in gigabytes"
+  type        = number
+  default     = 20
+}
+
+variable "engine_version" {
+  description = "The version of the database engine"
   type        = string
+  default     = "5.7"
+}
+
+variable "instance_class" {
+  description = "The instance type of the RDS instance"
+  type        = string
+  default     = "db.t3.micro"
+}
+
+variable "db_name" {
+  description = "The name of the database"
+  type        = string
+  default     = "mydb"
 }
 
 variable "username" {
-  description = "Database username"
+  description = "The username for the database"
   type        = string
 }
 
 variable "password" {
-  description = "Database password"
+  description = "The password for the database"
   type        = string
+  sensitive   = true
 }
 
-variable "subnet_ids" {
-  description = "List of subnet IDs for the RDS instance"
+variable "parameter_group_name" {
+  description = "The name of the DB parameter group"
+  type        = string
+  default     = "default.mysql5.7"
+}
+
+variable "db_subnet_group_name" {
+  description = "The DB subnet group name"
+  type        = string
+  default     = "default-subnet-group"
+}
+
+variable "vpc_security_group_ids" {
+  description = "A list of VPC security groups to associate"
   type        = list(string)
+  default     = []
 }
 
-variable "security_group_id" {
-  description = "Security group ID for the RDS instance"
-  type        = string
+variable "skip_final_snapshot" {
+  description = "Determines whether a final DB snapshot is created before the DB instance is deleted"
+  type        = bool
+  default     = true
+}
+
+variable "publicly_accessible" {
+  description = "Bool to control if instance is publicly accessible"
+  type        = bool
+  default     = false
+}
+
+variable "multi_az" {
+  description = "Specifies if the RDS instance is multi-AZ"
+  type        = bool
+  default     = false
+}
+
+variable "storage_encrypted" {
+  description = "Specifies whether the DB instance is encrypted"
+  type        = bool
+  default     = false
+}
+
+variable "backup_retention_period" {
+  description = "The number of days to retain backups"
+  type        = number
+  default     = 7
 }
