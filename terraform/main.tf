5136c0be3b2c58e54e31c00036c7329421ed5919
Rakan Asfour <102238945+rakanasfour@users.noreply.github.com>
2024-07-17 04:05:42 -0700

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,25 +1,30 @@
+provider "aws" {
+  region = "us-east-2"
+}
 module "vpc" {
   source      = "./modules/vpc"
   cidr_block  = "10.0.0.0/16"
   subnet_count = 2
 }
 
+module "rds" {
+  source = "./modules/rds"
+  username = var.username
+  password = var.password
+}
+
+output "db_instance_endpoint" {
+  value = module.rds.db_instance_endpoint
+}
+
+module "ecr" {
+  source = "./modules/ecr"
+}
+
+
 module "eks" {
   source         = "./modules/eks"
   cluster_name   = "rakan-cluster"
   cluster_role_arn = module.eks.cluster_role_arn
   subnet_ids     = module.vpc.subnet_ids
-}
-
-module "rds" {
-  source            = "./modules/rds"
-  db_name           = "my-mysql-db"
-  username          = "root"
-  password          = "Password"
-  subnet_ids        = module.vpc.subnet_ids
-  security_group_id = module.rds.security_group_id  
-}
-
-module "ecr" {
-  source = "./modules/ecr"
 }
